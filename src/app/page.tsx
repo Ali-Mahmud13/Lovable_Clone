a1ecce8565949ffe6493b5b471696425471a00de
Ali-Mahmud13 <alimahmud13032@gmail.com>
2025-10-11 21:34:44 +0500

    03: trpc sertup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,14 @@
+"use client";
+import { useTRPC } from "@/trpc/client";
+import {useQuery} from "@tanstack/react-query";
+
+const Page = () => {
+  const trpc = useTRPC();
+  const {data}= useQuery(trpc.createAI.queryOptions({text: "Hello!"}))
 
 
-const Page = async() => {
   return <div>
+    {JSON.stringify(data)}
   </div>
 };
 
