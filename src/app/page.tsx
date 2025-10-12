459d9298dd8be004342387758f189665ad6233a9
Ali-Mahmud13 <alimahmud13032@gmail.com>
2025-10-12 17:58:02 +0500

    05: Ai jobs setup done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,38 @@
 "use client";
 
+
 import {Button} from '@/components/ui/button';
 import { useTRPC } from '@/trpc/client';
 import { useMutation } from '@tanstack/react-query';
 import { toast } from 'sonner';
+import { useState } from 'react';
+import { Input } from '@/components/ui/input';
+
+
 
 
 const Page = () => {
 
-  const trpc=useTRPC();
-  const invoke=useMutation(trpc.invoke.mutationOptions({
-    onSuccess: ()=>{
-      toast.success("Background Job Invoked Successfully")
-    }
-  }));
 
-  return <div className='p-4 max-w-7xl mx-auto'>
-    <Button onClick={()=>invoke.mutate({text: "john"})}>  
-      Invoke Background Job
-    </Button>
-   
-  </div>
+ const [value,setValue]=useState("");
+
+
+ const trpc=useTRPC();
+ const invoke=useMutation(trpc.invoke.mutationOptions({
+   onSuccess: ()=>{
+     toast.success("Background Job Invoked Successfully")
+   }
+ }));
+
+
+ return <div className='p-4 max-w-7xl mx-auto'>
+   <Input value={value} onChange={(e)=>setValue(e.target.value)}/>
+   <Button onClick={()=>invoke.mutate({value: value})}> 
+     Invoke Background Job
+   </Button>
+ 
+ </div>
 };
+
 
 export default Page;
--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,17 +1,26 @@
 import { inngest } from "./client";
+import { createAgent, openai } from '@inngest/agent-kit';
+
+
+
 
 export const helloWorld = inngest.createFunction(
-  { id: "hello-world" },
-  { event: "test/hello.world" },
-  async ({ event, step }) => {
+ { id: "hello-world" },
+ { event: "test/hello.world" },
+ async ({ event, step }) => {
+  
+   const codeAgent = createAgent({
+ name: 'Code Agent',
+ system: "You are an expert Next.js developer. You write readable, mainainable code. You write simple Next.js and React snippets",
+ model: openai({model:'gpt-3.5-turbo'}), // Use GPT-4 for higher quality output
+});
+const { output } = await codeAgent.run(
+ `write the following snippet: ${event.data.value}`,
+);
+console.log(output);
+// [{ role: 'assistant', content: 'function removeUnecessaryWhitespace(...' }]
 
-    //imagine this is a download or some other long running task
-    await step.sleep("wait-a-moment", "10s");
 
-    //imagine this is a transcription or some other long running task
-    await step.sleep("wait-a-moment", "10s");
-
-    await step.sleep("wait-a-moment", "5s");
-    return { message: `Hello ${event.data.email}!` };
-  },
+    return { output };
+ },
 );
