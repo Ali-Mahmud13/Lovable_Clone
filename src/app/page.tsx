90d89137054190638fb0d01ce79d37305d377b22
Ali-Mahmud13 <alimahmud13032@gmail.com>
2025-10-12 10:58:34 +0500

    04: inngest setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,25 @@
 "use client";
-import { useTRPC } from "@/trpc/client";
-import {useQuery} from "@tanstack/react-query";
+
+import {Button} from '@/components/ui/button';
+import { useTRPC } from '@/trpc/client';
+import { useMutation } from '@tanstack/react-query';
+import { toast } from 'sonner';
+
 
 const Page = () => {
-  const trpc = useTRPC();
-  const {data}= useQuery(trpc.createAI.queryOptions({text: "Hello!"}))
 
+  const trpc=useTRPC();
+  const invoke=useMutation(trpc.invoke.mutationOptions({
+    onSuccess: ()=>{
+      toast.success("Background Job Invoked Successfully")
+    }
+  }));
 
-  return <div>
-    {JSON.stringify(data)}
+  return <div className='p-4 max-w-7xl mx-auto'>
+    <Button onClick={()=>invoke.mutate({text: "john"})}>  
+      Invoke Background Job
+    </Button>
+   
   </div>
 };
 
